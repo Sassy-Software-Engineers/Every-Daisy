720554e476b0863bb187be2fbe6991680e1fd355
Noelle Huchette <noelle.huchette@gmail.com>
2022-08-02 21:28:48 +0000

    basic associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,24 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require('./db');
+const User = require('./models/User');
+const Product = require('./models/Product');
+const Review = require('./models/Review');
+const Order = require('./models/Order');
 
-const User = require('./models/User')
-
-//associations could go here!
+User.hasMany(Order);
+Order.belongsTo(User);
+User.hasMany(Review);
+Review.belongsTo(User);
+Review.belongsTo(Product);
+Product.hasMany(Review);
 
 module.exports = {
   db,
   models: {
     User,
+    Order,
+    Product,
+    Review,
   },
-}
+};
