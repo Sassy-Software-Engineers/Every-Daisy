e9ca6a37b9451aa5367417905a944942408c644b
Noelle Huchette <noelle.huchette@gmail.com>
2022-08-03 21:25:12 +0000

    removed console log

--- server/api/users.js
+++ server/api/users.js
@@ -5,7 +5,6 @@ const Order = require('../db/models/Order');
 const requireToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
-    console.log(token)
     const user = await User.findByToken(token);
     req.user = user;
     next();
