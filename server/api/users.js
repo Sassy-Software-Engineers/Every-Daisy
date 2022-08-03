db38e9ad23c95599a7dba8be8b32539725b6a384
Amy Ji <amy.ji.1123@gmail.com>
2022-08-03 10:54:36 -0400

    get all users route

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { models: { User }} = require('../db')
+const User = require('../db/models/User')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -8,10 +8,10 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
+      attributes: ['id', 'name', 'address']
     })
     res.json(users)
-  } catch (err) {
-    next(err)
+  } catch (error) {
+    next(error)
   }
 })
