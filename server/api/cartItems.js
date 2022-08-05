e46ac882da1409c0a237adc7f2ea2fd83c7215e9
Fiona Murray <fiona.c.murray@gmail.com>
2022-08-04 22:19:08 -0400

    rough outline of cart express routes

--- server/api/cartItems.js
+++ server/api/cartItems.js
@@ -4,36 +4,52 @@ const {
 } = require('../db');
 const requireToken = require('./middlewares')
 
-router.get('/', async (req, res, next) => {
-    try {
-      const cartItems = await CartItem.findAll({
-        include: [Product,Order],
-      });
-  
-      res.json(cartItems);
-    } catch (e) {
-      next(e);
-    }
-  });
+// need a router.use in index for /cart
 
-  router.post('/', async (req, res, next) => {
-    try {
-      const order = await Order.create(req.body);
-      res.send(order);
-    } catch (e) {
-      next(e);
-    }
-  });
-
-  router.put('/', async (req, res, next) => {
+router.get('/:orderId', async (req, res, next) => {
     try {
       const cartItems = await CartItem.findAll({where: {
         orderId: req.params.orderId,
         },
         include: Order
     });
-      const updated = await cartItems.update(req.body);
+      res.json(cartItems);
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.post('/:productId', async (req, res, next) => {
+    try {
+      let product = await Product.findByPk(req.params.productId)
+      const order = await Order.create(req.body);
+      await order.addProduct(product.id, {through: {quantity: 1, price: product.price}})
+      res.send(order);
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.put('/:orderId', async (req, res, next) => {
+    // need this to recieve orderId and id of product to be able to update ?
+    // well see
+    try {
+      const order = await Order.findByPk(req.params.orderId)
+
       res.json(updated);
+    }
+    catch (error) {
+      next(error);
+    }
+  });
+
+  router.delete('/products/:orderId', async (req, res, next) => {
+    //this is to delete specific produts off of the cart, might need a separate if we want to delete whole cart
+    // need this to recieve orderId and id of product to be able to delete 
+    try {
+      const order = await Order.findByPk(req.params.orderId)
+
+      res.json(deleted);
     }
     catch (error) {
       next(error);
