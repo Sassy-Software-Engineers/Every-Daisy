7286de3ec385373aa38959455b0805ac3710776d
Amy Ji <amy.ji.1123@gmail.com>
2022-08-07 21:28:46 -0400

    review component

---
+++ client/components/Elements/Review.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+export class ProductReviews extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "",
+            content: "",
+        }
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.addReview(this.state)
+        this.setState({
+            title: "",
+            content: "",
+        });
+    }
+
+    render() {
+        const { title, content } = this.state;
+        const { handleSubmit, handleChange } = this;
+        return (
+            <div>
+                <form id="ProductReview-form" onSubmit={handleSubmit}>
+                    <label >Title:</label>
+                    <input type="text" name="title" value={title} onChange={handleChange} required />
+
+                    <label >Review:</label>
+                    <input type="text" name="review" value={content} onChange={handleChange} required />
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addReview: (review) => dispatch(addNewReview(review))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductReviews);
