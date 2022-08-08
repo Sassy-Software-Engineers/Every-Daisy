a819579f12b96d401104ce0179292632393fc066
Amy Ji <amy.ji.1123@gmail.com>
2022-08-07 23:10:18 -0400

    updated addReview component

---
+++ client/components/Elements/AllReviews.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+export class 
--- client/components/Product/SingleProduct.js
+++ client/components/Product/SingleProduct.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchProduct } from '../../store/products/singleProduct';
-import Review from "../Elements/Review"
+import Review from "../Elements/AddReview"
 
 class SingleProduct extends React.Component {
   componentDidMount() {
