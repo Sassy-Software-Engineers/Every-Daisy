b6d61b0bad92303f6586907d2acc915a5b9c200e
Fiona Murray <fiona.c.murray@gmail.com>
2022-08-09 11:53:31 -0400

    seed file updated

--- script/seed.js
+++ script/seed.js
@@ -2,7 +2,7 @@
 
 const {
   db,
-  models: { User, Order, Product, Category },
+  models: { User, Order, Product, Category, Review },
 } = require('../server/db');
 
 const users = [
@@ -44,7 +44,7 @@ const products = [
   {
     title: 'Prayer plant',
     description:
-      'The Calathea Orbifolia\'s broad, oval-shaped leaves with contrasting green stripes make it a popular plant for home decor and plant enthusiasts alike. Its lush foliage benefits from frequent waterings and high humidity. In ideal conditions (think tropical), its pet-friendly leaves can grow over a foot wide!',
+      "The Calathea Orbifolia's broad, oval-shaped leaves with contrasting green stripes make it a popular plant for home decor and plant enthusiasts alike. Its lush foliage benefits from frequent waterings and high humidity. In ideal conditions (think tropical), its pet-friendly leaves can grow over a foot wide!",
     price: 36.0,
     quantity: 100,
     image:
@@ -74,8 +74,7 @@ const products = [
       'Chlorophytum comosum, usually called spider plant or common spider plant due to its spider-like look, also known as spider ivy, ribbon plant, and hen and chickens is a species of evergreen perennial flowering plant of the family Asparagaceae.',
     price: 35.99,
     quantity: 100,
-    image:
-      'https://www.plantvine.com/plants/Variegated-Spider-Plant-2.jpg',
+    image: 'https://www.plantvine.com/plants/Variegated-Spider-Plant-2.jpg',
   },
   {
     title: 'Rattlesnake Plant',
@@ -83,22 +82,21 @@ const products = [
       'Rattlesnake plants are not generally considered the best option for a beginners houseplant collection. They have very particular heat, light, and moisture requirements. However, it can be worth the extra effort when you see the beautiful ornamental leaves the plant produces.',
     price: 15.99,
     quantity: 50,
-    image:
-      'https://h2.commercev3.net/cdn.brecks.com/images/800/76430P.jpg',
+    image: 'https://h2.commercev3.net/cdn.brecks.com/images/800/76430P.jpg',
   },
   {
     title: 'Venus FlyTrap',
     description:
-      'Not only is this plant pet-friendly, it\'s also super low-maintenance. Keep your Venus flytrap happy by placing it somewhere that gets at least four hours of direct sunlight and watering it with distilled water.',
+      "Not only is this plant pet-friendly, it's also super low-maintenance. Keep your Venus flytrap happy by placing it somewhere that gets at least four hours of direct sunlight and watering it with distilled water.",
     price: 10.99,
     quantity: 150,
     image:
       'https://themellowsf.com/wp-content/uploads/2020/10/Venus-Fly-Trap-The-Mellow-SF-1-e1602374721627.jpg',
   },
   {
-    title: 'Burro\'s Tail',
+    title: "Burro's Tail",
     description:
-      'The Burro\'s Tail, Donkey Tail, or Sedum morganianum is a cross between a cactus and succulent. These charmers are easy to take care of and actually thrive if you leave them alone.',
+      "The Burro's Tail, Donkey Tail, or Sedum morganianum is a cross between a cactus and succulent. These charmers are easy to take care of and actually thrive if you leave them alone.",
     price: 15.99,
     quantity: 150,
     image:
@@ -126,8 +124,41 @@ const categories = [
   { name: 'easy' },
   { name: 'advanced' },
   { name: 'cat-favorite' },
-  { name: 'carniverous' },
+  { name: 'carnivorous' },
   { name: 'succulent' },
+];
+const reviews = [
+  {
+    starRating: 4,
+    title: 'Amazing!',
+    content: 'best plant ever, i love it so much',
+    plantId: 6,
+  },
+  {
+    starRating: 5,
+    title: 'im so happy!',
+    content: 'my cat loves to play with this',
+    plantId: 1,
+  },
+  {
+    starRating: 3,
+    title: 'good but it died',
+    content: 'gave it too much water and it died but it was pretty!',
+    plantId: 3,
+  },
+  {
+    starRating: 4,
+    title: 'favorite thing',
+    content: 'love this plant almost as much as my pets!',
+    plantId: 3,
+  },
+  {
+    starRating: 5,
+    title: 'need more of these',
+    content: 'gonna buy more and fill my whole house with these<3',
+    plantId: 7,
+  },
+
 ];
 async function seed() {
   await db.sync({ force: true });
@@ -151,16 +182,40 @@ async function seed() {
   const [newCategories] = await Promise.all(
     categories.map((c) => Category.create(c))
   );
-  
-// newProducts[0].addCategory(newCategories[0])
-// newProducts[1].setCategories(newCategories[2])
-// newProducts[2].setCategories(newCategories[2])
-// newProducts[3].setCategories(newCategories[1],newCategories[3])
-// newProducts[4].setCategories(newCategories[1],newCategories[3])
-// newProducts[5].setCategories(newCategories[2],newCategories[4])
-// newProducts[6].setCategories(newCategories[3],newCategories[6])
-// newProducts[7].setCategories(newCategories[3],newCategories[7])
+  const [newReviews] = await Promise.all(
+    reviews.map((c) => Review.create(c))
+  );
 
+  let herb = await Category.findOne({ where: { name: 'herb' } });
+  let flowering = await Category.findOne({ where: { name: 'flowering' } });
+  let tropical = await Category.findOne({ where: { name: 'tropical' } });
+  let easy = await Category.findOne({ where: { name: 'easy' } });
+  let advanced = await Category.findOne({ where: { name: 'advanced' } });
+  let catFavorite = await Category.findOne({ where: { name: 'cat-favorite' } });
+  let carnivorous = await Category.findOne({ where: { name: 'carnivorous' } });
+  let succulent = await Category.findOne({ where: { name: 'succulent' } });
+
+  let catnip = await Product.findOne({ where: { title: 'Catnip' } });
+  let prayer = await Product.findOne({ where: { title: 'Prayer plant' } });
+  let Bromeliad = await Product.findOne({
+    where: { title: 'Bromeliad Antonio Pink' },
+  });
+  let Mothorchid = await Product.findOne({ where: { title: 'Moth orchid' } });
+  let Spider = await Product.findOne({ where: { title: 'Spider Plant' } });
+  let Rattlesnake = await Product.findOne({
+    where: { title: 'Rattlesnake Plant' },
+  });
+  let Venus = await Product.findOne({ where: { title: 'Venus FlyTrap' } });
+  let Burro = await Product.findOne({ where: { title: "Burro's Tail" } });
+
+  await catnip.setCategories([herb, easy, catFavorite]);
+  await prayer.setCategories([tropical]);
+  await Bromeliad.setCategories([tropical]);
+  await Mothorchid.setCategories([flowering, easy]);
+  await Spider.setCategories([easy]);
+  await Rattlesnake.setCategories([advanced, tropical]);
+  await Venus.setCategories([carnivorous, easy]);
+  await Burro.setCategories([succulent, easy]);
 
   console.log(
     `seeded ${users.length} users, ${products.length} products, ${orders.length} orders, ${categories.length} categories`
@@ -169,7 +224,8 @@ async function seed() {
     newUsers.dataValues,
     newProducts.dataValues,
     newOrders.dataValues,
-    newCategories.dataValues
+    newCategories.dataValues,
+    newReviews.dataValues
   );
 }
 
@@ -191,4 +247,3 @@ if (module === require.main) {
   runSeed();
 }
 module.exports = seed;
-
