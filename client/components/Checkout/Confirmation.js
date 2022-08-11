9dd407f8912c95d3e22762c74a201d7783fda133
Kathryn McCrum <kathryncmccrum@gmail.com>
2022-08-10 21:14:01 -0400

    text added to confirmation page

--- client/components/Checkout/Confirmation.js
+++ client/components/Checkout/Confirmation.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Confirmation extends React.Component {
   render() {
-    return <div>Payment Submitted!</div>;
+    return (
+      <div className="confirmation-container">
+        <h1 style={{ textAlign: 'center' }}> Thank you for your order! </h1>
+        <div className="confirmation-div">
+          <h1 style={{ textAlign: 'center' }}>
+            YOUR ORDER WAS PLACED SUCCESSFULLY
+          </h1>
+        </div>
+        <h3 style={{ textAlign: 'center' }}>
+          Your Order Number: {Math.floor(Math.random() * Math.floor(9999))}
+        </h3>
+      </div>
+    );
   }
 }
 
